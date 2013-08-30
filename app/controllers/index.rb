eba8ce2b5d3947ca13b4166e7b19955a377b09fc
Alex Fiore <writefiore@gmail.com>
2013-08-30 15:59:52 -0700

    added bcrypt

--- app/controllers/index.rb
+++
@@ -1,9 +0,0 @@
-- root route that goes to a view showing login_logout buttons 
-- 
-
-
-
-get '/' do
-  # Look in app/views/index.erb
-  erb :index
-end
--- app/controllers/user_login.rb
+++ app/controllers/user_login.rb
@@ -7,10 +7,33 @@ get '/signup' do
 end
 
 post '/' do
-  #user profile 
+  
+  @this_user = User.find_by_email(params[:email])
+  
+  if @this_user.nil? 
+    redirect '/'
+  elsif
+    @this_user.password != params[:password]
+    redirect '/'
+  else
+    session[:id] = @this_user.id
+    erb :user_profile
+  end
+
 end
 
 post '/signup' do
-  #user profile 
+  @user = User.create(params)
+  if @user.valid? 
+    session[:id] = @user.id
+    redirect '/user_profile/:id'
+  else
+    @errors = @user.errors.full_messages
+    erb :signup
+  end
+end
+
+post '/user_profile' do
+  
 end
 
