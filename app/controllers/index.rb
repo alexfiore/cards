8faaa93345b3303fb6b2c936d329a659a37dc6a4
Alex Fiore <writefiore@gmail.com>
2013-08-30 15:01:10 -0700

    login/signup views and controller skeleton

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,8 @@
+- root route that goes to a view showing login_logout buttons 
+- 
+
+
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
---
+++ app/controllers/user_login.rb
@@ -0,0 +1,16 @@
+get '/' do 
+  erb :login
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/' do
+  #user profile 
+end
+
+post '/signup' do
+  #user profile 
+end
+
