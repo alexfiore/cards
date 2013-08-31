c8626bf1136f2f56b78dbda849f4ebd3bf3222a4
DBC-Apprentice <dbc@dbc.com>
2013-08-31 15:43:58 -0700

    implement deal card method in round model. the last commit was the controller actually.

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,25 @@ class Round < ActiveRecord::Base
   belongs_to :user
   has_many :guesses
   belongs_to :deck
+
+  def deal_card
+    self.deck.cards.first
+    # we want to deal a random card from this round's deck
+    # the card should have nil user_guess...
+
+    # selects the guess objects associated with 
+    # the cards that are in 
+    # my current round's deck
+
+    current_round_guesses = Guess.where(card_id: [self.deck.cards.map(&:id)], round_id: self.id)
+    already_dealt_cards = Card.where(id: [current_round_guesses.map(&:card_id)])
+    unguessed_cards = self.deck.cards - already_dealt_cards
+    unguessed_cards.sample 
+
+  end
+
+  def complete?
+    deal_card.nil?
+  end
+
 end
