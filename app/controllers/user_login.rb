422711c75754439f5d80d857d4dfe3808bae0f06
Jimmy <jimwong1023@gmail.com>
2013-08-31 21:24:27 -0700

    Added uncompleted game continuation feature

--- app/controllers/user_login.rb
+++ app/controllers/user_login.rb
@@ -13,30 +13,54 @@ post '/login' do
     redirect '/'
   elsif User.authenticate(params[:user][:email], params[:user][:password])
     session[:id] = @user.id
-    redirect "/user_profile/#{@user.id}"
+    redirect "/user_profile"
   else
     redirect '/'
   end
 end
 
-before '/user_profile/:id' do
+before '/user_profile' do
   unless current_user
     redirect '/'
   end
 end
 
-get '/user_profile/:id' do  #if current user is not log in then redirect them to '/'
+get '/user_profile' do  #if current user is not log in then redirect them to '/'
   @decks = Deck.all
   erb :user_profile
 end
 
+get '/user_profile/:id/history' do
+  user = User.find(params[:id])
+
+  @completed_rounds = []
+  user.rounds.each do |round|
+    if round.complete?
+      @completed_rounds << round
+    end
+  end
+
+  erb :history
+end
+
+get '/user_profile/:id/uncompleted_rounds' do
+  user = User.find(params[:id])
+  @uncompleted_rounds = []
+  user.rounds.each do |round|
+    unless round.complete?
+      @uncompleted_rounds << round
+    end
+  end
+
+  erb :uncompleted
+end
 
 post '/signup' do
   @user = User.new(params[:user])
 
   if @user.save 
     session[:id] = @user.id
-    redirect "/user_profile/#{@user.id}"
+    redirect "/user_profile"
   else
     @errors = @user.errors.full_messages
     erb :index
